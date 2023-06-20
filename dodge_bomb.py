dbe6da603f8c62d1633fb0680e4bd1758f48d557
c0b22130 <c0b22130d6@edu.teu.ac.jp>
2023-06-20 14:38:07 +0900

    練習3

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -4,7 +4,7 @@ import pygame as pg
 
 
 WIDTH, HEIGHT = 1600, 900
-
+delta={pg.K_UP:(0,-5),pg.K_DOWN:(0,+5),pg.K_LEFT:(-5,0),pg.K_RIGHT:(+5,0)}
 
 def main():
     pg.display.set_caption("逃げろ！こうかとん")
@@ -12,6 +12,8 @@ def main():
     bg_img = pg.image.load("ex02/fig/pg_bg.jpg")
     kk_img = pg.image.load("ex02/fig/3.png")
     kk_img = pg.transform.rotozoom(kk_img, 0, 2.0)
+    kk_rct=kk_img.get_rect()
+    kk_rct.center=900,400
     bomb=pg.Surface((20,20))
     pg.draw.circle(bomb,(255,0,0),(10,10),10)
     x=random.randint(0,WIDTH)
@@ -26,10 +28,16 @@ def main():
         for event in pg.event.get():
             if event.type == pg.QUIT: 
                 return
-            
+        key_lst=pg.key.get_pressed()
+        sum_mv=[0,0]
+        for k,mv in delta.items():
+            if key_lst[k]:
+                sum_mv[0]+=mv[0]
+                sum_mv[1]+=mv[1]
+        kk_rct.move_ip(sum_mv)
 
         screen.blit(bg_img, [0, 0])
-        screen.blit(kk_img, [900, 400])
+        screen.blit(kk_img, kk_rct)
         bomb_rct.move_ip(vx,vy)
         screen.blit(bomb,bomb_rct)
         pg.display.update()
